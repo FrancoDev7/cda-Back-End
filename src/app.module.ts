203a3f04f2c2455699f415bb3ec439c9cb6e4479
Francisco Pereira <francisco.pereira16@inacapmail.cl>
2024-09-17 20:39:58 -0300

    Finaliza Feauture de imagen Exito ✅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 import { ArticulosModule } from './articulos/articulos.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { FilesModule } from './files/files.module';
 
 
 @Module({
@@ -26,11 +29,17 @@ import { SeedModule } from './seed/seed.module';
 
     }),
 
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'static'),
+    }),
+
     ArticulosModule,
 
     CommonModule,
 
     SeedModule,
+
+    FilesModule,
   ],
   controllers: [],
   providers: [],
