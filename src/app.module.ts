fc78ec5605a0e8ecfc3ab5dfb29b15c740f774db
Francisco Pereira <francisco.pereira16@inacapmail.cl>
2024-09-17 20:52:55 -0300

    Feature Auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { ArticulosModule } from './articulos/articulos.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { FilesModule } from './files/files.module';
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
@@ -40,6 +41,8 @@ import { FilesModule } from './files/files.module';
     SeedModule,
 
     FilesModule,
+
+    AuthModule,
   ],
   controllers: [],
   providers: [],
